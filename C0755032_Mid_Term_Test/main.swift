7e852fc582009ee6da61efb8461c4fb13949135b
MacStudent <macstudent@126-iMac07.local>
2019-02-20 18:26:21 -0500

    Initial Commit

---
+++ C0755032_Mid_Term_Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0755032_Mid_Term_Test
+//
+//  Created by MacStudent on 2019-02-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
